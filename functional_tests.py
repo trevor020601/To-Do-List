96f45b3ec1e785d1dafedc968c1a8b8a7ef144e5
Trevor Karl <trevorkarl5@gmail.com>
2023-03-07 18:09:56 -0600

    Adding Unittest to Test Browser

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-assert 'Django' in browser.title
+    def quit(self):
+        self.browser.quit()
+
+    def test_open_list(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
